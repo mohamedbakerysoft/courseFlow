41d49e66bf6f0004617ff8a109c3ed0f4fbdc4c2
Mohamed Mahdy <mohamed.bakerysoft@gmail.com>
2025-12-28 10:33:49 +0200

    activate/disable right click

--- server/app/Actions/Install/RunMigrationsAction.php
+++ server/app/Actions/Install/RunMigrationsAction.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Install;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 
 class RunMigrationsAction
 {
@@ -13,6 +14,22 @@ class RunMigrationsAction
             Artisan::call('cache:clear');
             Artisan::call('migrate', ['--force' => true]);
             Artisan::call('db:seed', ['--force' => true]);
+            try {
+                Artisan::call('storage:link');
+            } catch (\Throwable $e) {
+            }
+            try {
+                Storage::disk('public')->makeDirectory('logos');
+            } catch (\Throwable $e) {
+            }
+            try {
+                Storage::disk('public')->makeDirectory('landing');
+            } catch (\Throwable $e) {
+            }
+            try {
+                Storage::disk('public')->makeDirectory('hero');
+            } catch (\Throwable $e) {
+            }
 
             return ['ok' => true];
         } catch (\Throwable $e) {
